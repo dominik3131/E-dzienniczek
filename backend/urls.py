ccab67473bd6b5a8d276d5f3c596c00f13809d6f
Dominik <rakdominikrak@gmail.com>
2020-11-08 13:59:38 +0100

    futher admin page adjustments

--- backend/urls.py
+++ backend/urls.py
@@ -18,5 +18,5 @@ from django.urls import path
 from .views import index
 urlpatterns = [
     path('', index, name='index'),
-    # path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
 ]
