f68e44b4098dcd922059c19f4b783600dd099b1e
Dominik <rakdominikrak@gmail.com>
2020-11-08 13:10:41 +0100

    initial backend models for users, classes, subjects and grades

---
+++ backend/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import *
+from django.contrib.admin.views.main import ChangeList
+
+admin.site.register(Student)
+admin.site.register(Administrator)
+admin.site.register(Teacher)
+admin.site.register(Parent)
+admin.site.register(SchoolClass)
+admin.site.register(Grade)
+admin.site.register(Subject)
