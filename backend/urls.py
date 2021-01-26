434e4caf5ee85603df519e11cb69c70e18077084
Dominik <rakdominikrak@gmail.com>
2021-01-26 22:05:25 +0100

    urls

--- backend/urls.py
+++ backend/urls.py
@@ -23,7 +23,9 @@ urlpatterns = [
     path('teachers', index, name='index'),
     path('teachers/<int:pk>', index, name='index'),
     path('admin', admin.site.urls),
-
+    path('users/*', index, name='index'),
+    path('users', index, name='index'),
+    
     path('api/students', StudentsList.as_view()),
     path('api/students/<int:pk>', StudentDetail.as_view()),
     path('api/parents', ParentsList.as_view()),
